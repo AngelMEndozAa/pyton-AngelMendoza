3f4e7f01482cb17515f5dc0e26a81bf1063f2db0
AngelMEndozAa <81320772+AngelMEndozAa@users.noreply.github.com>
2021-04-20 13:11:08 -0500

    Create palceholders.py

---
+++ palceholders.py
@@ -0,0 +1,12 @@
+#palceholders.py
+
+name = "Angel"
+age = "22"
+city = "Cordoba"
+state = "Veracruz"
+country = "Mexico"
+mesage = "my name is %s, I'm &s years old \n"
+print mesage%(name, age)
+
+mesage = "\t My name is %s \n\t I am %s years old \n\t I am from %s %s, %s"
+print mesage%(name,age,city,state,country)
