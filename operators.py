ae4aeed8486df82ad5b5950aa595f8ba88088e98
AngelMEndozAa <81320772+AngelMEndozAa@users.noreply.github.com>
2021-04-20 13:25:33 -0500

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,4 +1,32 @@
-adn = "ASDFGHJKLAGAGDAERTKALALAAHSASAGA"
-print adn[0:3]
-print adn[len(adn):]
-print adn[:]
+age1 = 9.0
+age2 = 20.0
+
+print "age1 = ", age1
+print "age2 = ", age2
+
+sumage = age1 + age2
+print "Suma (+) : ", sumage
+print "Resta (-) : ", age1 - age2
+print "Multiplica (*) : " , age1 * age2
+print "Dividir (/) : ", age1 / age2
+print "Residuo (%) : ", age2 % age1
+
+message = "today was a beatiful day"
+firstname = "Angel"
+lastname = "Mendoza"
+
+print message
+print firstname
+print lastname
+
+print "sum  strings (+) ", message + " " + firstname + " " + lastname
+
+adn = "AACTGTGAACTAACGTCTTAACGACTGGTACGTAC"
+print "adn = ", adn
+print " adn pos 0 ", adn[0]
+print " adn pos 4 ", adn[4]
+print " adn pos 0 to 7 ", adn[0:7]
+print " adn pos 10 to 14 ", adn[10:14]
+print " adn first 10 ", adn[:10]
+print " adn length - 2 ", adn[:-2]
+
