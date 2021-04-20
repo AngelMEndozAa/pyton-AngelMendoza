ef5bffea25a8113e75696057aa6f0bfe6b8f54bc
AngelMEndozAa <81320772+AngelMEndozAa@users.noreply.github.com>
2021-04-20 13:00:57 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+# variables.py
+# autor: Mendoza Santos Angel
+# Marzo, 3, 2021
+
+year = 2020 
+sentence = "My name is Angel" 
+sarah, bob, mike = 16,20,25 
+name, age = "mary", 24 
+PI = 2.1416
+e = 2.17
+
+print("year = ", year)
+print PI
+print e 
